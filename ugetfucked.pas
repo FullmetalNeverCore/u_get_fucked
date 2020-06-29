4d87ddb4186314f02f93352c1c9b76304c9e428a
FullmetalNeverCore <60969891+FullmetalNeverCore@users.noreply.github.com>
2020-06-30 02:01:27 +0300

    day2

---
+++ ugetfucked.pas
@@ -0,0 +1,11 @@
+﻿program u_get_fucked;
+var 
+  a : string;
+  
+begin
+  read(a);
+  if (a <> a) then 
+    writeln('U get fucked!')
+  else 
+    writeln('U get fucked!') 
+end.
---
+++ ugetfucked.py
@@ -0,0 +1,7 @@
+ugetfucked = ''
+
+while ugetfucked != 'I get fucked!':
+    ugetfucked = str(input('U get fucked'))
+
+print("U get fucked")
+input("Enter to quit")
